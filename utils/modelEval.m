b9872108db5e2f9176d6c5ca4c046058e04fa6fd
Le Duc Thang <le.duc.thang0312@gmail.com>
2024-04-11 19:09:34 +0900

    Delete utils directory

--- utils/modelEval.m
+++
@@ -1,5 +0,0 @@
-% DNN forward mode
-function loss = modelEval(dlnet,dlX,Y)
-    dlYPred = forward(dlnet,dlX);
-    loss = crossentropy(dlYPred,Y);
-end
--- utils/modelGradients.m
+++
@@ -1,6 +0,0 @@
-% DNN backward mode
-function [gradients,loss] = modelGradients(dlnet,dlX,Y)
-    dlYPred = forward(dlnet,dlX);
-    loss = crossentropy(dlYPred,Y);
-    gradients = dlgradient(loss,dlnet.Learnables);
-end
